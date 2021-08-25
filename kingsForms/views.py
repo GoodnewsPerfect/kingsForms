6b353464fe5b400f868e6c8fc2342523ccf7dc2d
GoodnewsPerfect <samuelgoodnews15@gmail.com>
2021-08-25 11:56:15 +0100

    Adjustments to the Zone category

--- kingsForms/views.py
+++ kingsForms/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'kingsForms/home.html')
+    return HttpResponse("<center style='background-color: lightgrey'><h1>Thank you for contacting me! </h1></center>")
